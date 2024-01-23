e1c166b2d807096055dbc49defe8d58275d8b734
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-23 13:43:45 +0100

    add PrismaModule

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // wichtig, damit andere Module den Service importieren können
+})
+export class PrismaModule {}
