3318275d5e6761d1f0de7be4c4ddd1672b5d80ce
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-23 15:33:21 +0100

    Add error handling for duplicate email in user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -16,21 +17,30 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     // save the new user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      // select: {
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      // },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
 
-    delete user.hash; // dirty solution for now, will fix this later
+      delete user.hash; // dirty solution for now, will fix this later
 
-    // return the saved user without the hash
-    return user;
+      // return the saved user without the hash
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 }
