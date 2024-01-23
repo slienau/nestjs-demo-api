b1b737e01a617ceca5130bac336c84de52ddaa4e
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-23 15:15:42 +0100

    implement signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
@@ -10,8 +11,26 @@ export class AuthService {
     return 'login';
   }
 
-  signup(dto: AuthDto) {
-    console.log('signup', { dto });
-    return 'signup';
+  async signup(dto: AuthDto) {
+    // generate hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the database
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // select: {
+      //   id: true,
+      //   email: true,
+      //   createdAt: true,
+      // },
+    });
+
+    delete user.hash; // dirty solution for now, will fix this later
+
+    // return the saved user without the hash
+    return user;
   }
 }
