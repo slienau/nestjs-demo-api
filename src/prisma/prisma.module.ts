1909d34c6812c2b40e0260fd59024409ab8625c7
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-23 13:49:19 +0100

    make PrismaService global

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // macht den Service global (in allen Modulen) verfügbar; Er muss nicht extra importiert werden
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService], // wichtig, damit andere Module den Service importieren können
+  exports: [PrismaService], // wichtig, damit andere Module den Service zugreifen können (auch bei globalem modul!)
 })
 export class PrismaModule {}
