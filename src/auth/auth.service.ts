e7d7b6bac4e6eb07d72e7c17808dfb825a8f4220
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-23 15:09:54 +0100

    add validation pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
@@ -9,7 +10,8 @@ export class AuthService {
     return 'login';
   }
 
-  signup() {
+  signup(dto: AuthDto) {
+    console.log('signup', { dto });
     return 'signup';
   }
 }
